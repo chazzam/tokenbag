17ccaee6054ec5d5c9ef3af09538c4559b50f437
Chazz <6433463+chazzam@users.noreply.github.com>
2024-03-26 20:19:59 -0500

    Create main.py
    initial attempt at running

---
+++ main.py
@@ -0,0 +1,59 @@
+import sys
+
+if sys.version_info.major < 3 or sys.version_info.minor < 10:
+  print("Python 3.10 or higher is required.")
+  sys.exit(1)
+
+import argparse
+from tokenbag import tokenbag
+
+
+def main():
+  parser = argparse.ArgumentParser(
+      description="Tokenbag: a simple token bag for Python.")
+  #parser.add_argument("-v",
+  #                    "--version",
+  #                    action="version",
+  #                    version=f"{tokenbag.__title__} {tokenbag.__version__}")
+  parser.add_argument("-c",
+                      "--config",
+                      help="Path to the configuration file. [bagpool.conf]",
+                      default="bagpool.conf")
+  parser.add_argument("-d",
+                      "--debug",
+                      action="store_true",
+                      help="Enable debug mode.")
+  parser.add_argument("-l", "--log", help="Path to the log file.")
+  parser.add_argument(
+      "-r",
+      "--rank",
+      help=
+      "Max rank of the skill. 0 - no rank, 1 - Bronze, 2 - Silver, 3 - Gold. [3]",
+      type=int,
+      default=3)
+  parser.add_argument(
+      "-n",
+      "--number-of-draws",
+      help="Number of draws to make for generating statistics. [100]",
+      type=int,
+      default=100)
+  parser.add_argument(
+      "-p",
+      "--pull-cap",
+      help=
+      "Maximum number of tokens to pull in a single draw. 0 - unlimited [10]",
+      type=int,
+      default=10)
+  parser.add_argument("-b",
+                      "--bag",
+                      help="Specify name of bag (if more than one in config)",
+                      type=str,
+                      default="Base")
+  args = parser.parse_args()
+  #tokenpulls = tokenbag
+  #pool = tokenpulls.build_pool(args.config)
+  pool = tokenbag.build_pool(args.config, args.bag)
+  print(pool)
+
+
+main()
